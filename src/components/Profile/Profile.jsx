5c8b64046fecda887273ed3f9f28d4fe0e5e8fd6
VladyslavPiatak <vladislawatson@gmail.com>
2024-04-28 15:58:07 +0300

    init Profile FriendList and TransactionHisttory

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import User from "./ProfileUser.module"
+import Status from "./ProfileStatus.module"
+
+export default function Profile() {
+  return (
+    <>
+      <User />
+      <Stats/>
+    </>
+  )
+}
